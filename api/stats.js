2d336f3d2a482e79db31e1182f0f2e4fe4b79b2c
google-com-ops <kharilazmi54@gmail.com>
2026-02-08 22:07:39 +0700

    Update report.js

--- api/stats.js
+++ api/stats.js
@@ -1,14 +1,33 @@
-let requests = [];
+let logs = [];
 
-export default function handler(req, res) {
-  const now = new Date().toLocaleTimeString();
+const BOT_TOKEN = "8203281432:AAGM0aze8Dt8CPXzW4svTzBw3ZB-iLt2GRs";
+const CHAT_ID = "8468685075";
 
-  const randomValue = Math.floor(Math.random() * 100);
-  requests.push({ time: now, value: randomValue });
+export default async function handler(req, res) {
 
-  if (requests.length > 20) {
-    requests.shift();
+  const now = new Date().toISOString();
+  const ip = req.headers["x-forwarded-for"] || "unknown";
+
+  logs.push({ time: now, ip });
+
+  if (logs.length > 100) {
+    logs.shift();
   }
 
-  res.status(200).json(requests);
+  // kirim ke telegram tiap dipanggil
+  const text = `Report 10 menit\nTotal Request: ${logs.length}`;
+
+  await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text
+    })
+  });
+
+  res.status(200).json({
+    total: logs.length,
+    data: logs
+  });
 }
